4d51ada729ebb5a259467836e89322e79b169cb6
Ikvoel <husen.seino@gmail.com>
2026-05-22 22:19:50 +0700

    name

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -63,7 +63,7 @@ export function Hero() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.4 }}
           >
-            Muhammad Nur Husein
+            Kairo
           </motion.h1>
 
           {/* Role Stack */}
